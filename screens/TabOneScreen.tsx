4fe392219df510f2ad12b105b93d11975aa78f48
Le Ngoc Tan Toan <tantoan1909@gmail.com>
2022-06-30 16:48:36 +0700

    Chat Item

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,7 +1,55 @@
 import * as React from 'react';
-import {Text } from 'react-native';
+import { Text, Image, View, StyleSheet } from 'react-native';
+
 export default function TabOneScreen() {
   return (
-    <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Image source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg' }} style={styles.image} />
+      <View style={styles.rightContainer}>
+        <View style={styles.row}>
+          <Text style={styles.name}>Van Loc</Text>
+          <Text style={styles.text}>11:11 AM</Text>
+        </View>
+        <Text style={styles.text}>Hello </Text>
+      </View>
+    </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    padding: 5,
+    margin: 5,
+    borderRadius: 15,
+    borderShadow: 1,
+    backgroundColor: 'yellow',
+  },
+  image: {
+    height: 50,
+    width: 50,
+    borderRadius: 30,
+    marginRigth: 10,
+  },
+  rightContainer:{
+    padding: 5,
+    flex: 1,
+    justifyContent: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  name: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginBottom: 3,
+  },
+  text: {
+    color: 'grey',
+    fontSize: 15,
+  }
+});
+
+
+
