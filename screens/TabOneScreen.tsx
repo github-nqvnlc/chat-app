acfd88f3e0fe062d666d58bb04c0ce36c03d0840
Le Ngoc Tan Toan <tantoan1909@gmail.com>
2022-07-01 16:30:28 +0700

    List chat rooms item

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,54 +1,26 @@
 import * as React from 'react';
-import { Text, Image, View, StyleSheet } from 'react-native';
+import { Text, Image, View, StyleSheet, FlatList } from 'react-native';
+import ChatRoomItem from '../components/ChatRoomItem';
+
+import chatRoomsData from '../assets/dummy-data/ChatRooms';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg' }} style={styles.image} />
-      <View style={styles.rightContainer}>
-        <View style={styles.row}>
-          <Text style={styles.name}>Van Loc</Text>
-          <Text style={styles.text}>11:11 AM</Text>
-        </View>
-        <Text style={styles.text}>Hello </Text>
-      </View>
+    <View style={styles.page}>
+      <FlatList 
+        data={chatRoomsData}
+        renderItem = {({item}) => <ChatRoomItem chatRoom={item} />}
+        showsVerticalScrollIndicator = {false}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    padding: 5,
-    margin: 5,
-    borderRadius: 15,
-    borderShadow: 1,
-    backgroundColor: 'yellow',
-  },
-  image: {
-    height: 50,
-    width: 50,
-    borderRadius: 30,
-    marginRigth: 10,
-  },
-  rightContainer:{
-    padding: 5,
-    flex: 1,
-    justifyContent: 'center',
-  },
-  row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  name: {
-    fontSize: 15,
-    fontWeight: 'bold',
-    marginBottom: 3,
-  },
-  text: {
-    color: 'grey',
-    fontSize: 15,
-  }
+ page: {
+  backgroundColor: 'white',
+  flex: 1,
+ }
 });
 
 
